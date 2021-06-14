94554443a248e85de0c81015917c5113821982eb
Spissini <jose.gangaspissini@gmail.com>
2021-06-14 18:41:35 -0400

    l

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class EMPRETAIL(models.Model):
+    num_Tarjeta = models.IntegerField(primary_key=True, verbose_name='Numero de Tarjeta')
+    nom_Emp = models.CharField(max_length=15, verbose_name='Nombre de la Empresa')
+    def __str__(self):
+        return self.num_Tarjeta
+
+class APORTADOR(models.Model):
+    rut_Aport = models.IntegerField(primary_key=True, verbose_name='Rut del Apoortador')
+    nomb_Aport = models.CharField(max_length=15, verbose_name='Nombre del Aportador')
+    apelli_Aport = models.CharField(max_length=25, verbose_name='Apellidos del Aportador')
+    num_Aport = models.IntegerField(verbose_name='Numero Telefonico del Aportador')
+    correo_Aport = models.CharField(max_length=45, verbose_name='Correo del Aportador')
+    def __str__(self):
+        return self.rut_Aport
+
+class APORTE(models.Model):
+    id_Aporte = models.IntegerField(primary_key=True, verbose_name='ID Del Aporte')
+    cant_Aporte = models.IntegerField(verbose_name='Cantidad del Aporte')
+    aportador = models.ForeignKey(APORTADOR, on_delete=models.CASCADE)
+    empretail = models.ForeignKey(EMPRETAIL, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.id_Aporte
+
